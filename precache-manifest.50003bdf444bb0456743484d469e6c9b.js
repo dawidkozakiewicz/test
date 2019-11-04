4985fac995027111e3237f6aa37edac7b55be583
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-11-04 23:13:29 +0100

    Updates

---
+++ precache-manifest.50003bdf444bb0456743484d469e6c9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc6ff59aff438de8981e67d1f95ee8d3",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "dd6ad340a863f99a2cc5",
+    "url": "/test/static/css/main.d6d694b6.chunk.css"
+  },
+  {
+    "revision": "01e754695c946889f2df",
+    "url": "/test/static/js/2.10946c00.chunk.js"
+  },
+  {
+    "revision": "dd6ad340a863f99a2cc5",
+    "url": "/test/static/js/main.1b29ab94.chunk.js"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  },
+  {
+    "revision": "57887880cccd58ab296b832e699db7ed",
+    "url": "/test/static/media/winter-icons.57887880.gif"
+  }
+]);
